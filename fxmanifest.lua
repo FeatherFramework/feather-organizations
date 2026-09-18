aa3ccb43d2847f66278bd1d4e62378f177cf1767
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-09-17 21:52:57 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 name 'feather-organizations'
 author 'Feather Framework'
 description 'Durable organization identity for the Feather Framework'
-version '0.1.0'
+version '0.1.1'
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
